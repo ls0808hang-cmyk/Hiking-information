acdbdbf2a86eae9d997b02a8dadf15fb05e3c0f8
ls0808hang <ls0808hang@gmail.com>
2026-04-15 07:40:19 +0000

    refactor: optimize service key encoding in proxy

--- functions/api/search.js
+++ functions/api/search.js
@@ -2,7 +2,7 @@ export async function onRequestGet(context) {
   const { searchParams } = new URL(context.request.url);
   const mountainName = searchParams.get('searchWrd') || '';
 
-  const SERVICE_KEY = 'c6d38e7f40238d4d5ee29ef0a05b4f884d1d294a56c3e61a5dfc8f04a0f7b696';
+  const SERVICE_KEY = encodeURIComponent('c6d38e7f40238d4d5ee29ef0a05b4f884d1d294a56c3e61a5dfc8f04a0f7b696');
 
   const apiUrl = `https://apis.data.go.kr/1400000/service/cultureInfoService2/mntInfoOpenAPI2?serviceKey=${SERVICE_KEY}&searchWrd=${encodeURIComponent(mountainName)}&_type=json`;
 
